0e79d451f6f46fa9ad84a46625b3f298c9d81eea
qaloop <agiletester786@gmail.com>
2019-01-11 18:33:09 +0530

    Updated tests

--- config/globalConfig.js
+++ config/globalConfig.js
@@ -1,9 +1,4 @@
 module.exports = {
     googleURL: 'https://google.com',
     gitHubURL: 'https://github.com/qaloop',
-    gitHubRepoURL: 'https://github.com/qaloop?tab=repositories',
-    globalJestTimeout: 'jest.setTimeout(30000)',
-    globalSetup: './setup.js',
-    globalTeardown: './teardown.js',
-    testEnvironment: './puppeteer_environment.js',
 };
